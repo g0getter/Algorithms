7863234000a4f266f8d841b02ca25598ec9a0475
나경 <nagyeong.yeo1023@gmail.com>
2024-08-07 16:21:12 +0900

    Create a project, Height Checker folder(1051)

---
+++ Algorithms/1051_HeightChecker/HeightChecker.swift
@@ -0,0 +1,55 @@
+//
+//  HeightChecker.swift
+//  Algorithms
+//
+//  Created by 여나경 on 8/7/24.
+//
+
+import Foundation
+
+// First written on 2024/8/2
+class HeightChecker: Solution {
+    func heightChecker(_ heights: [Int]) -> Int {
+        var count = 0
+        // let expected = heights.sorted() // Sol1. Use built-in sort func
+        let expected = bubbleSort(heights) // Sol2. Use Bubble sort
+        // let expec
+        
+        for (index, height) in heights.enumerated() {
+            if height != expected[index] {
+                count += 1
+            }
+        }
+        return count
+    }
+    
+    private func bubbleSort(_ list: [Int]) -> [Int] {
+        var sortedList = list
+        
+        // outer loop indicating where the inner loop starts
+        for i in 0..<sortedList.count-1 {
+            // inner loop to swap elements
+            for j in 0..<sortedList.count-i-1 {
+                if sortedList[j] > sortedList[j+1] {
+                    let temp = sortedList[j]
+                    sortedList[j] = sortedList[j+1]
+                    sortedList[j+1] = temp
+                }
+            }
+            // print(sortedList)
+        }
+        
+        return sortedList
+    }
+    
+    private func mergeSort(_ list: [Int]) -> [Int] {
+        let result: [Int] = []
+        // 1. Divide into two halves
+        // 2. Sort each half, starting from the smallest half whose has only one member
+        // 3. Merge the sorted half
+        // ------ DONE RECURSIVELY -----
+        return result
+    }
+    
+    
+}
---
+++ Algorithms/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Algorithms
+//
+//  Created by 여나경 on 8/7/24.
+//
+
+import Foundation
+
+//print("Hello, World!")
+
+let input = [1,1,4,2,1,3]
+let result = HeightChecker().heightChecker(input)
+print("input: \(input)\nresult: \(result)\n")
+
+protocol Solution {}
