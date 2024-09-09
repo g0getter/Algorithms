1050c9fc80313fa39d75bd3f2a8367e2b7896513
나경 <nagyeong.yeo1023@gmail.com>
2024-09-09 12:49:36 +0900

    905. Sort Array By Parity(2)

--- Algorithms/905_SortArrayByParity/SortArrayByParity.swift
+++ Algorithms/905_SortArrayByParity/SortArrayByParity.swift
@@ -8,8 +8,8 @@
 import Foundation
 class SortArrayByParity: Solution {
     func sortArrayByParity(_ nums: [Int]) -> [Int] {
-        snowballSolution(nums)
-        // pushToTheBack(nums) // FIXME: Debug
+//        snowballSolution(nums)
+         pushToTheBack(nums)
     }
     
     // move odd numbers to back from the beginning
@@ -32,39 +32,38 @@ class SortArrayByParity: Solution {
         return nums
     }
     
-    // move odd numbers to back from the end
-    // FIXME: Debug
+    // move odd numbers to back
+    // Time Complexity: O(n), Space Complexity: O(1) but pretty slow..  
     func pushToTheBack(_ nums: [Int]) -> [Int] {
-        var oddIndexToSwap = nums.count-1
-        var nums = nums
+        guard nums.count > 1 else { return nums }
         
-        for i in 0..<nums.count {
-            if i >= oddIndexToSwap {
-                break
-            }
+        var evenIndexToSwap = calculateEvenIndexToSwap(nums, nums.count)
+        guard evenIndexToSwap >= 0 else { return nums }
+        
+        var nums = nums
+        var i = 0
+        
+        while i < evenIndexToSwap {
             if nums[i] % 2 == 1 {
-                // (i) swap
+                // swap
                 let temp = nums[i]
-                nums[i] = nums[oddIndexToSwap]
-                nums[oddIndexToSwap] = temp
-                // oddIndexToSwap -= 1
-                oddIndexToSwap = calculateOddIndexToSwap(nums, oddIndexToSwap)
-                print(oddIndexToSwap)
+                nums[i] = nums[evenIndexToSwap]
+                nums[evenIndexToSwap] = temp
+                evenIndexToSwap = calculateEvenIndexToSwap(nums, evenIndexToSwap)
             }
+            i += 1
         }
         
         return nums
     }
 
-    func calculateOddIndexToSwap(_ nums: [Int], _ currentOddIndexToSwap: Int) -> Int {
-        var nextOddIndex = currentOddIndexToSwap - 1
-        guard nextOddIndex >= 0 else { return -1 }
+    func calculateEvenIndexToSwap(_ nums: [Int], _ currentEvenIndexToSwap: Int) -> Int {
+        var nextEvenIndex = currentEvenIndexToSwap - 1
         
-        while nums[nextOddIndex] % 2 == 1 && nextOddIndex >= 0 {
-            nextOddIndex -= 1
-            print(nextOddIndex)
+        while nextEvenIndex >= 0, nums[nextEvenIndex] % 2 == 1 {
+            nextEvenIndex -= 1
         }
         
-        return nextOddIndex
+        return nextEvenIndex
     }
 }
