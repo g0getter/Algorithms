b7851425a8905f4c3ddb258a4b3c6749a6af12f5
나경 <nagyeong.yeo1023@gmail.com>
2024-09-23 11:40:16 +0900

    12909. 올바른 괄호(upgraded)

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.swift
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.swift
@@ -4,6 +4,16 @@ func solution(_ s:String) -> Bool
 {
     var ans:Bool = false
     // Sol1. 정수 변수 생성, 0 되면 true, 음수가 한 번이라도 나오면 즉시 false
+    ans = solution1(s)
+    
+    // Sol2. ( 전용 스택 생성, ) 나오면 pop, 뺄 게 없는 경우 즉시 false
+    // ans = solution2(s)
+    return ans
+}
+
+/// Sol1. 정수 변수 생성, 0 되면 true, 음수가 한 번이라도 나오면 즉시 false
+func solution1(_ s:String) -> Bool {
+    
     var parenthesisNum = 0
     
     for char in s {
@@ -17,8 +27,20 @@ func solution(_ s:String) -> Bool
         }
     }
     
-    ans = (parenthesisNum == 0)
+    return parenthesisNum == 0
+}
+
+/// Sol2. ( 전용 스택 생성, ) 나오면 pop, 뺄 게 없는 경우 즉시 false
+func solution2(_ s:String) -> Bool {
+    var parenthesisStack: [Character] = []
     
-    // Sol2. ( 전용 스택 생성, ) 나오면 pop, 뺄 게 없는 경우 즉시 false
-    return ans
+    for char in s {
+        if char == "(" {
+            parenthesisStack.append(char)
+        } else {
+            guard parenthesisStack.popLast() != nil else { return false }
+        }
+    }
+
+    return parenthesisStack.count == 0
 }
