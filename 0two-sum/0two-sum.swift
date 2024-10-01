80d1245bf97fbe1aa96f78345111d873a007a93c
Aiden <84381320+g0getter@users.noreply.github.com>
2024-10-01 16:49:31 +0900

    Time: 42 ms, Memory: 16.4 MB - LeetHub

--- 0two-sum/0two-sum.swift
+++ 0two-sum/0two-sum.swift
@@ -1,7 +1,12 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        // 1. Most simple, easy way: O(n^2) time complexity - Find each pair and return if it exists.
-        // (brute force way)
+        // solution1(nums, target)
+        solution2(nums, target)
+    }
+    
+    // 1. Most simple, easy way: O(n^2) time complexity - Find each pair and return if it exists.
+    // (brute force way)
+    func solution1(_ nums: [Int], _ target: Int) -> [Int] {
         var i = 0
         var j = 1
         
@@ -14,6 +19,22 @@ class Solution {
             j = i + 1
         }
         
+        return []
+    }
+    
+    // 2. Use a Hashmap
+    func solution2(_ nums: [Int], _ target: Int) -> [Int] {
+        // var partnerHashMap: [Int: Int] = [:] // [<index>: <value>]
+        var partnerHashMap: [Int: Int] = [:] // [<value>: <index>]
+        
+        for (index, num) in nums.enumerated() {
+            partnerHashMap[target - num] = index
+        }
+        print(partnerHashMap)
+        for (index, num) in nums.enumerated() {
+            guard let partnerIndex = partnerHashMap[num], index != partnerIndex else { continue }
+            return [index, partnerIndex]
+        }
         return []
     }
 }
