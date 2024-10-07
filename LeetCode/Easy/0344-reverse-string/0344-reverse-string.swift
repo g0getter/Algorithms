df17cb38485a17a8692bab5bdbb13a147f59734a
Aiden <84381320+g0getter@users.noreply.github.com>
2024-10-07 15:44:18 +0900

    Time: 116 ms (32.91%), Space: 20.2 MB (72.01%) - LeetHub

--- LeetCode/Easy/0344-reverse-string/0344-reverse-string.swift
+++ LeetCode/Easy/0344-reverse-string/0344-reverse-string.swift
@@ -1,5 +1,11 @@
 class Solution {
     func reverseString(_ s: inout [Character]) {
+        // solution1(&s)
+        solution2(&s)
+    }
+    
+    // Insert and remove others
+    func solution1(_ s: inout [Character]) {
         let length = s.count
         
         for i in 0..<length {
@@ -9,5 +15,19 @@ class Solution {
         }
     
         s.removeFirst(length)
+    }
+    
+    // Swap with two pointers
+    func solution2(_ s: inout [Character]) {
+        var i = 0
+        var j = s.count - 1
+        
+        while (i < j) {
+            let temp = s[i]
+            s[i] = s[j]
+            s[j] = temp
+            i += 1
+            j -= 1
+        }
     }
 }
