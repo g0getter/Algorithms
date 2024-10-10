f43fdad76df384dc296e54ba2b751866cfe4d531
Aiden <84381320+g0getter@users.noreply.github.com>
2024-10-10 19:47:50 +0900

    [level 2] Title: 타겟 넘버, Time: 264.62 ms, Memory: 27.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
@@ -3,6 +3,7 @@ import Foundation
 func solution(_ numbers:[Int], _ target:Int) -> Int {
     // [X] 전체 합에서 target을 빼고 남은 짝수를 반반 분리하고 그 안에서 정렬하는 경우의 수
     // [O] tree + BFS. 모든 경우의 수를 leaves에 넣고 target과 같은 개수 반환
+    // TODO: DFS
     
     var leaves = [0]
     var count = 0
