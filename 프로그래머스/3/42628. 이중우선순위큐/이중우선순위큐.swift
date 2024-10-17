b11b29b3ac52a333544e74f871345e114b77c167
Aiden <84381320+g0getter@users.noreply.github.com>
2024-10-17 20:02:55 +0900

    [level 3] Title: 이중우선순위큐, Time: 2991.59 ms, Memory: 21.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+func solution(_ operations:[String]) -> [Int] {
+    // queue
+    // 1. insert - insert
+    // 2. D 1, D -1 -> sort & delete
+    var queue: [Int] = []
+    
+    for operation in operations {
+        // let action = operation.first ?? Character("")
+        // let value = operation.suffix(from: 2)
+        let action = operation.split(separator: " ").map { String($0) }.first ?? ""
+        let value = operation.split(separator: " ").map { String($0) }.last ?? ""
+        // print(value)
+        if action == "I" {
+            queue.append(Int(value) ?? 0)
+        } else { // "D"
+            guard queue.count > 0 else { continue } // 빈 큐이면 연산 무시
+            queue.sort()
+            
+            if value == "1" {
+                queue.removeLast()
+            } else { // value == "-1"
+                queue.removeFirst()
+            }
+        }
+        // print(queue)
+    }
+    
+    guard let max = queue.max(),
+    let min = queue.min() else { return [0, 0] }
+    return [max, min]
+}
+
+// 이중 우선순위 큐 구현 - heap 이용
+// heap 구현: struct
+// func heapSolution(_ operations:[String]) -> [Int] {}
+// struct MaxHeap: Heap {
+//     func insert() {
+        
+//     }
+// } 
