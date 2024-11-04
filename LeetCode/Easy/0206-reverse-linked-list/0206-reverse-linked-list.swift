e6f0fd2785a2426ad59360be94996ca699a524ee
Aiden <84381320+g0getter@users.noreply.github.com>
2024-11-04 17:35:43 +0900

    Time: 0 ms (100%), Space: 18.1 MB (6.77%) - LeetHub

---
+++ LeetCode/Easy/0206-reverse-linked-list/0206-reverse-linked-list.swift
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+
+extension ListNode {
+    // Deep copy
+    func copy(with zone: NSZone? = nil) -> Any {
+        return ListNode(self.val, self.next)
+    }
+}
+
+class Solution {
+    
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        guard let head = head else { return nil }
+        
+        /// node which will be the `next` and was the previous one
+        var previousNode: ListNode? = nil
+        /// currentNode to modify
+        var currentNode: ListNode? = head.copy() as! ListNode
+        
+        // head?.next = nil // 주소값 복사..하네..
+        
+        while currentNode != nil {
+            var temp = currentNode?.next?.copy() as? ListNode
+            currentNode?.next = previousNode?.copy() as? ListNode
+            previousNode = currentNode?.copy() as? ListNode
+            currentNode = temp?.copy() as? ListNode
+        }
+        return previousNode
+    }
+}
+
+// TODO: Implement recursively
