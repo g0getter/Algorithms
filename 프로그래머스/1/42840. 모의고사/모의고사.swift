952ad3191c725d1e40340191072bce24aec69e4c
Aiden <84381320+g0getter@users.noreply.github.com>
2024-11-11 17:40:47 +0900

    [level 1] Title: 모의고사, Time: 7.44 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.swift
+++ 프로그래머스/1/42840. 모의고사/모의고사.swift
@@ -1,6 +1,41 @@
 import Foundation
 
 func solution(_ answers:[Int]) -> [Int] {
+    // firstSolution(answers)
+    secondSolution(answers)
+}
+
+// answerCounts -> dictionary 사용
+private func secondSolution(_ answers:[Int]) -> [Int] {
+    // 1, 2, 3 최소 배열 각각 생성
+    let firstRule = [1, 2, 3, 4, 5]
+    let secondRule = [2, 1, 2, 3, 2, 4, 2, 5]
+    let thirdRule = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
+    var answerCounts = [1: 0, 2: 0, 3: 0]
+    
+    // answers 돌면서 각 사람별 정답 개수 증가
+    for (i, answer) in answers.enumerated() {
+        if answer == firstRule[i%firstRule.count] {
+            answerCounts[1]! += 1
+        }
+        if answer == secondRule[i%secondRule.count] {
+            answerCounts[2]! += 1
+        }
+        if answer == thirdRule[i%thirdRule.count] {
+            answerCounts[3]! += 1
+        }
+    }
+    
+    // max()와 같은 index로 이루어진 배열 만들기(+오름차순 정렬)
+    let winners = answerCounts.sorted { $0.key < $1.key }
+    .filter { $0.value == answerCounts.values.max() }
+    .map { $0.key }
+
+    return winners
+}
+
+private func firstSolution(_ answers:[Int]) -> [Int] {
     // 1, 2, 3 최소 배열 각각 생성
     let firstRule = [1, 2, 3, 4, 5]
     let secondRule = [2, 1, 2, 3, 2, 4, 2, 5]
