be63f637e53f4a74545602983b206f7aedc66eba
Aiden <84381320+g0getter@users.noreply.github.com>
2025-01-25 13:00:31 +0900

    Time: 13 ms (5%), Space: 20.7 MB (6.25%) - LeetHub

---
+++ LeetCode/Medium/0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.swift
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    // 1. dfs or bfs? bfs. 한 level 탐색할 때마다 자료구조에 새로 담기.
+    // 자료구조 - 큐(fifo 위해)
+    // 왠지 dfs는 재귀일듯
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        guard let root = root else { return [] }
+        
+        var result: [[Int]] = [[root.val]]
+        var newNodes: [TreeNode] = [root]
+        
+        while(newNodes.count > 0) { // TODO: 발전시키기
+            newNodes = getSubnodesOfThese(newNodes)
+            guard newNodes.count > 0 else { return result }
+            let newValues = newNodes.map { $0.val }
+            result.append(newValues)
+        }
+        
+        return result
+    }
+    
+    func getSubnodesOfThese(_ nodes: [TreeNode]) -> [TreeNode] {
+        
+        let flattenedSubnodes = nodes.flatMap { [$0.left, $0.right] }.compactMap{$0}
+        print(flattenedSubnodes)
+        
+        return flattenedSubnodes
+    }
+}
