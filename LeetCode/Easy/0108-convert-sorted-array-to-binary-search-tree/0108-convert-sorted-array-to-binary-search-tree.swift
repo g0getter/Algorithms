3f28292d3d8172a3a49c131ad63ee54ec6b38d74
Aiden <84381320+g0getter@users.noreply.github.com>
2025-01-27 11:36:14 +0900

    Time: 34 ms (2.29%), Space: 20.3 MB (13.74%) - LeetHub

--- LeetCode/Easy/0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.swift
+++ LeetCode/Easy/0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.swift
@@ -13,46 +13,30 @@
  *     }
  * }
  */
+
+ 
 class Solution {
+
+    // divide and conquer, recursion 사용
+    // 1. 중간 것 찾음
+    // 2. 중간에 두고, 나머지를 두 부분으로 나눔
+    // 3. 나머지 2개에 대해 1-2 반복
+    // 4. 나머지들의 길이가 1이 되면 return
     func sortedArrayToBST(_ nums: [Int]) -> TreeNode? {
-        // divide and conquer, recursion 사용
-        // 1. 중간 것 찾음
-        // 2. 중간에 두고, 나머지를 두 부분으로 나눔
-        // 3. 나머지 2개에 대해 1-2 반복
-        // 4. 나머지들의 길이가 1이 되면 return
-        findMidAndGetTwoSubs(nums)
-    }
-    
-    func findMidAndGetTwoSubs(_ nums: [Int]) -> TreeNode? {
         guard !nums.isEmpty else { return nil }
+
+        let midIndex = nums.count / 2 // index of root node
         
-        let midIndex = nums.count / 2
-        
-        // *반환 조건* 양쪽 길이가 1인지 확인
-        if midIndex == 1 { // 앞쪽은 뒤쪽 길이보다 1 길거나 같음
-            let left = TreeNode(nums[0])
-            var right: TreeNode? = nil
-            if midIndex+1 < nums.count { // 뒤쪽이 있으면
-                right = TreeNode(nums[midIndex+1])
-            }
-            
-            return TreeNode(nums[midIndex], left, right)
-        }
-        
-        // 아직 더 돌려야 함
-        let leftNums = Array(nums[0..<midIndex])
-        var rightNums: [Int] = []
-        if midIndex+1 < nums.count { // 뒤쪽이 있으면
-            rightNums = Array(nums[(midIndex+1)...])
-        }
-        
-        let left = findMidAndGetTwoSubs(leftNums)
-        let right = findMidAndGetTwoSubs(rightNums)
-        
-        // print(leftNums)
-        // print(rightNums)
-        
+        // let left = TreeNode(~~) // left node
+        let left = sortedArrayToBST(Array(nums[0..<midIndex]))
+        // let right = TreeNode(~~~) // right node
+        // let right = sortedArrayToBST(Array(nums[(midIndex+1)...])) // midIndex+1 should not exceed nums.count -> when?? ONLY WHEN the length is less than or equal to 2
+        let right = sortedArrayToBST(Array(nums[(midIndex+1)...]))
+        // 가능한 이유는, midIndex+1이 아무리 커봤지 nums.count를 넘지 않기 때문이다. 
+
+        print("nums[(<\(midIndex)>+1)..<\(nums.count)]): \(nums[(midIndex+1)..<nums.count]))")
+
+        // print("!!!! 2..<2:\(2..<2)") // (!!!같은 수 ..<같은 수 까지는 에러 안 남. 앞이 커지면 문제지만.)
         return TreeNode(nums[midIndex], left, right)
-        
     }
 }
