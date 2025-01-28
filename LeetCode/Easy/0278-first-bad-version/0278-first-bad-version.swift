87ce2ab2488f3166ad69b32aa3c7977cb434a4db
Aiden <84381320+g0getter@users.noreply.github.com>
2025-01-28 11:52:17 +0900

    Time: 3 ms (67.46%), Space: 17 MB (21.05%) - LeetHub

--- LeetCode/Easy/0278-first-bad-version/0278-first-bad-version.swift
+++ LeetCode/Easy/0278-first-bad-version/0278-first-bad-version.swift
@@ -4,8 +4,27 @@
  */
 
 class Solution : VersionControl {
-    // 이진탐색 - mid, oldMid가 아니라 left, right로 시도
+    // 이진탐색(2) - 마지막에 left, right T/F 비교 조건문 생략
     func firstBadVersion(_ n: Int) -> Int {
+        var left = 1 //
+        var right = n
+        
+        guard isBadVersion(left) == false else { return 1 }
+
+        while(right-left>1) {
+            let mid = left+(right-left)/2
+            if isBadVersion(mid) { // search a left half
+                right = mid
+            } else { // right half
+                left = mid
+            }
+        }
+        
+        return right
+    }
+    
+    // 이진탐색(1) - mid, oldMid가 아니라 left, right로 시도
+    func firstBadVersion_1(_ n: Int) -> Int {
         var left = 1
         var right = n
         
