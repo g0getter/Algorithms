39f28932bec0ff31076c71a6491ed5857af0b4dc
Aiden <84381320+g0getter@users.noreply.github.com>
2025-01-28 13:02:53 +0900

    Time: 3 ms (67.46%), Space: 17.2 MB (17.22%) - LeetHub

--- LeetCode/Easy/0278-first-bad-version/0278-first-bad-version.swift
+++ LeetCode/Easy/0278-first-bad-version/0278-first-bad-version.swift
@@ -4,9 +4,29 @@
  */
 
 class Solution : VersionControl {
-    // 이진탐색(2) - 마지막에 left, right T/F 비교 조건문 생략
+
+    // 이진탐색(3) - while 조건 left<right로 설정
     func firstBadVersion(_ n: Int) -> Int {
-        var left = 1 //
+        var left = 1
+        var right = n
+        
+        while(left<right) {
+            let mid = left+(right-left)/2
+            if isBadVersion(mid) { // search a left half
+                right = mid
+            } else { // right half
+                left = mid + 1 // 왜냐면, F~F는 탐색할 구간이 아님. ()
+            }
+        }
+        
+        return right
+    }
+
+    // 이진탐색(2) - 마지막에 left, right T/F 비교 조건문 생략
+    // - 처음부터 T인 것만 아니라면 left는 항상 F, right는 항상 T인 값이므로.
+    // (왜냐면 애초부터 로직이 mid가 T인지 F인지에 따라 mid를 right 혹은 left로 바꾸는 것이므로)
+    func firstBadVersion_2(_ n: Int) -> Int {
+        var left = 1
         var right = n
         
         guard isBadVersion(left) == false else { return 1 }
