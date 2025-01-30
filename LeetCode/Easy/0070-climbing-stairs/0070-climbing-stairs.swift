a627f1ca62ab6105ba2a5e48f4e8550206e457f2
Aiden <84381320+g0getter@users.noreply.github.com>
2025-01-30 10:46:24 +0900

    Time: 0 ms (100%), Space: 20.1 MB (6.75%) - LeetHub

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.swift
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.swift
@@ -4,16 +4,16 @@ class Solution {
         guard n != 1 else { return 1 }
         guard n != 2 else { return 2 }
         
-        var stepStack = [1, 2] // step1, step2
+        var stepQueue = [1, 2] // step1, step2
         
         for level in 3...n {
-            let sum = stepStack[0] + stepStack[1]
-            stepStack.removeFirst()
-            stepStack.append(sum)
+            let sum = stepQueue[0] + stepQueue[1]
+            stepQueue.removeFirst()
+            stepQueue.append(sum)
         }
         
-        guard stepStack.count == 2 else { return -1 }
+        // guard stepQueue.count == 2 else { return -1 }
         
-        return stepStack.last ?? -1
+        return stepQueue.last ?? -1
     }
 }
