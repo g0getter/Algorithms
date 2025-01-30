38043d280679faed397c4b278f42cb8544298d8c
Aiden <84381320+g0getter@users.noreply.github.com>
2025-01-30 10:55:22 +0900

    Time: 0 ms (100%), Space: 19.8 MB (6.75%) - LeetHub

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.swift
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.swift
@@ -1,12 +1,12 @@
 class Solution {
-    func climbStairs(_ n: Int) -> Int {
+    func climbStairs_new(_ n: Int) -> Int {
         // W(n+2) = W(n) + W(n+1) (n>=1)
         guard n != 1 else { return 1 }
         guard n != 2 else { return 2 }
         
         var stepQueue = [1, 2] // step1, step2
         
-        for level in 3...n {
+        for _ in 3...n {
             let sum = stepQueue[0] + stepQueue[1]
             stepQueue.removeFirst()
             stepQueue.append(sum)
@@ -15,5 +15,24 @@ class Solution {
         // guard stepQueue.count == 2 else { return -1 }
         
         return stepQueue.last ?? -1
+    }
+
+    // wasting memory version
+    func climbStairs(_ n: Int) -> Int {
+        // W(n+2) = W(n) + W(n+1) (n>=1)
+        guard n != 1 else { return 1 }
+        guard n != 2 else { return 2 }
+        
+        var steps = Array(repeating: 0, count: n+1)
+        
+        steps[0] = -1 // not used
+        steps[1] = 1
+        steps[2] = 2
+        
+        for level in 3...n {
+            steps[level] = steps[level-2] + steps[level-1]
+        }
+        
+        return steps[n]
     }
 }
