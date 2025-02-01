35fe800aabc64381ada8d89e5ae792a8a8a551a6
Aiden <84381320+g0getter@users.noreply.github.com>
2025-02-01 09:42:26 +0900

    Time: 9 ms (5.62%), Space: 22.1 MB (5.62%) - LeetHub

--- LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.swift
+++ LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.swift
@@ -1,8 +1,24 @@
 class Solution {
+    // Solution(2)
+    // not to use .max() at the end
+    func maxSubArray(_ nums: [Int]) -> Int {
+        var maxSubs = [nums[0]] // S
+        var maxSubSoFar = nums[0]
+        
+        for i in 1..<nums.count {
+            maxSubs.append(max(maxSubs[i-1]+nums[i], nums[i]))
+            maxSubSoFar = max(maxSubs[i], maxSubSoFar)
+        }
+        
+        return maxSubSoFar
+    }
+
+    // Solution(1)
+    // O(n) solution
     // S(k): maximum SubArray including nums[k]
     // S(k) = max(S(k-1)+nums[k], nums[k]) (k>=1, S(0) = nums[0])
     // answer: max(S)
-    func maxSubArray(_ nums: [Int]) -> Int {
+    func maxSubArray_Kadane(_ nums: [Int]) -> Int {
         var maxSubs = [nums[0]] // S
         
         for i in 1..<nums.count {
