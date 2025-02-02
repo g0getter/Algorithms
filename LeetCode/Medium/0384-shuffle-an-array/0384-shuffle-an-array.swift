30bf2505f7f6d8623bf85310420d5930abb910ff
Aiden <84381320+g0getter@users.noreply.github.com>
2025-02-02 11:25:01 +0900

    Time: 24 ms (52.63%), Space: 21.4 MB (7.02%) - LeetHub

--- LeetCode/Medium/0384-shuffle-an-array/0384-shuffle-an-array.swift
+++ LeetCode/Medium/0384-shuffle-an-array/0384-shuffle-an-array.swift
@@ -1,7 +1,8 @@
-
+// Solution 1(Fisher-Yates Algorithm): return an array with a loop generating a random number for each index and remove the index from the original array -> high time complexity O(n^2)(due to .remove(index))
+// Solution 2: return an array with a loop generating a random number for each index and SWAP with the index from the original array -> linear time complexity O(n)
 class Solution {
 
-    let originalNums: [Int]
+    private let originalNums: [Int]
     
     init(_ nums: [Int]) {
         originalNums = nums
@@ -12,13 +13,34 @@ class Solution {
     }
     
     func shuffle() -> [Int] {
-        originalNums.shuffled()
+        // Solution 1: Fisher-Yates Algorithm
+        var shuffledNums: [Int] = []
+        var nums = originalNums
+        for _ in originalNums {
+            let i = Int.random(in: 0..<nums.count)
+            shuffledNums.append(nums[i])
+            nums.remove(at: i)
+        }
+
+        return shuffledNums
+        
+        // Solution 2: Fisher-Yates > modern method(Durstenfeld's/Knuth's)
+        // var nums = originalNums
+        // for i in (1..<nums.count).reversed() {
+        //     let randomI = Int.random(in: 0..<i)
+        //     // swap
+        //     let temp = nums[i]
+        //     nums[i] = nums[randomI]
+        //     nums[randomI] = temp
+        // }
+        
+        // return nums
     }
 }
 
 /**
  * Your Solution object will be instantiated and called as such:
  * let obj = Solution(nums)
- * let ret_1: [Int] = obj.reset()
  * let ret_2: [Int] = obj.shuffle()
+ * let ret_1: [Int] = obj.reset()
  */
