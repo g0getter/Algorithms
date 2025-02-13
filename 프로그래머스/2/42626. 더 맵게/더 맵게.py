0bd7b0fcc3cdf09cf28d60ccf6e5bea32c61e7fd
Aiden <84381320+g0getter@users.noreply.github.com>
2025-02-13 16:41:03 +0900

    [level 2] Title: 더 맵게, Time: 1879.74 ms, Memory: 51.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,39 @@
+import heapq
+
+def solution(scoville, K):
+    
+    answer = 0
+    heapq.heapify(scoville) # scoville: mutable!!
+    
+    minValue = scoville[0]
+    
+    if minValue >= K:
+        return answer
+    
+    while minValue < K and len(scoville) > 1:
+        # firstMin = min(scoville)
+        # scoville.remove(firstMin)
+        firstMin = heapq.heappop(scoville)
+        
+        # secondMin = min(scoville)
+        # scoville.remove(secondMin)
+        secondMin = heapq.heappop(scoville)
+        
+        newValue = firstMin + secondMin * 2
+        # scoville.append(newValue)
+        heapq.heappush(scoville, newValue)
+        
+        # minValue = heapq.heappop(scoville)
+        minValue = scoville[0]
+        answer += 1
+        
+    if len(scoville) == 1 and minValue < K:
+        return -1
+    
+    return answer
+
+# 1. 가장 작은 것끼리 연산
+# 2. min이 이상인지 확인, 아니면 min 2개에 대해 다시 연산
+# 반복
+
+# 복잡도 줄이기: 연산한 값을 오름차순으로 자리에 맞게 삽입?
