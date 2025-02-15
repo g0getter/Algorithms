b0ea4e958ac47543dc1d1eb3506c96158b20fe2b
Aiden <84381320+g0getter@users.noreply.github.com>
2025-02-16 08:23:19 +0900

    Time: 2885 ms (5.97%), Space: 55.2 MB (11.94%) - LeetHub

--- LeetCode/Medium/0204-count-primes/0204-count-primes.swift
+++ LeetCode/Medium/0204-count-primes/0204-count-primes.swift
@@ -3,7 +3,7 @@ class Solution {
     func countPrimes(_ n: Int) -> Int {
         // 2부터 시작, 2 배수 전부 제외
         // 그 다음 수 N부터 시작, N 배수 전부 제외
-        // -> 끝까지 가면 끝
+        // -> root N까지 가면 끝
         
         guard n > 2 else { return 0 }
         guard n != 3 else { return 1 }
@@ -14,47 +14,40 @@ class Solution {
         
         for num in 2...intRootN {
             guard mutableList.contains(num) else { continue }
-            // print(num)
             mutableList.removeAll { $0 % num == 0 && $0 != num }
-            // print(mutableList)
         }
-        // for num in 2...intRootN {
-        //     // print(num)
-        //     mutableList.removeAll { $0 % num == 0 && $0 != num }
-        //     // print(mutableList)
-        // }
         
         return mutableList.count
     }
     
     // Runtime : SO LONG
-    func countPrimes_checkAll(_ n: Int) -> Int {
-        var primesCount = 0
+    // func countPrimes_checkAll(_ n: Int) -> Int {
+    //     var primesCount = 0
         
-        guard n > 2 else { return 0 }
+    //     guard n > 2 else { return 0 }
         
-        for num in 2..<n {
-            if isPrime(num) {
-                primesCount += 1
-            }
-        }
+    //     for num in 2..<n {
+    //         if isPrime(num) {
+    //             primesCount += 1
+    //         }
+    //     }
         
-        return primesCount
-    }
+    //     return primesCount
+    // }
     
-    // n > 2
-    private func isPrime(_ n: Int) -> Bool {
-        guard n >= 4 else { return true }
-        let rootN = Double(n).squareRoot()
-        let intRootN = floor(rootN)
+    // // n > 2
+    // private func isPrime(_ n: Int) -> Bool {
+    //     guard n >= 4 else { return true }
+    //     let rootN = Double(n).squareRoot()
+    //     let intRootN = floor(rootN)
         
-        // 소인수가 존재하면 return false
-        for num in 2...Int(intRootN) {
-            if n % num == 0 {
-                return false
-            }
-        }
+    //     // 소인수가 존재하면 return false
+    //     for num in 2...Int(intRootN) {
+    //         if n % num == 0 {
+    //             return false
+    //         }
+    //     }
         
-        return true
-    }
+    //     return true
+    // }
 }
