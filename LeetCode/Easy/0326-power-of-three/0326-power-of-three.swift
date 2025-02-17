91823dc18e62fe447570bd33b474edad31b5f6bd
Aiden <84381320+g0getter@users.noreply.github.com>
2025-02-17 22:00:35 +0900

    Time: 3 ms (66.67%), Space: 20.3 MB (21.74%) - LeetHub

--- LeetCode/Easy/0326-power-of-three/0326-power-of-three.swift
+++ LeetCode/Easy/0326-power-of-three/0326-power-of-three.swift
@@ -12,7 +12,11 @@ class Solution {
     }
     
     func logBase(_ x: Double, base: Double) -> Double {
-        return log(x) / log(base)
+        return log10(x) / log10(base)
+        // logBase에서 자연로그 아닌 상용로그(log10) 사용하면 `return Int(exactly: result) != nil` 사용 가능.
+        // 왜냐하면, e는 무리수라서 이진법으로 표현 불가능 -> 부동소수점 연산 시 정확한 값으로 표현할 수 없어 오차 발생
+        // * 부동소수점 발생 이유: '이진법으로 정확하게 표현할 수 없는 숫자'는 근사값으로 저장해야 하므로 발생.
+        // 따라서 10진수를 다룰 때는 log10이 더 정확한 경우가 많음(하지만 절대적으로 보장하는 것은 아님)
     }
 }
 
