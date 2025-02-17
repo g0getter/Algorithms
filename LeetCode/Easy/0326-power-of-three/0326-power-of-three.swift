e0fdca57aef21a18b582b2051ad234b9b9ea0911
Aiden <84381320+g0getter@users.noreply.github.com>
2025-02-17 21:44:39 +0900

    Time: 7 ms (28.99%), Space: 20.6 MB (13.04%) - LeetHub

--- LeetCode/Easy/0326-power-of-three/0326-power-of-three.swift
+++ LeetCode/Easy/0326-power-of-three/0326-power-of-three.swift
@@ -5,11 +5,10 @@ class Solution {
         
         let result = logBase(Double(n), base: 3)
         // 부동소수점 오차 방지
+        // (return Int(exactly: result) != nil 사용 불가)
+        // n=243 -> result=4.999999 나옴
         let roundedResult = round(result)
         return pow(3, roundedResult) == Double(n)
-        // print(result)
-        // print(Int(exactly: result))
-        // return Int(exactly: result) != nil // if Double is exactly Int
     }
     
     func logBase(_ x: Double, base: Double) -> Double {
