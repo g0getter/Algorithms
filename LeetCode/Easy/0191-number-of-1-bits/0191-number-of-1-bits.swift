750e13330d111a2ea61ee6f6ebfd6181f1c41d4e
Aiden <84381320+g0getter@users.noreply.github.com>
2025-02-18 07:20:40 +0900

    Time: 0 ms (100%), Space: 19.5 MB (22.9%) - LeetHub

--- LeetCode/Easy/0191-number-of-1-bits/0191-number-of-1-bits.swift
+++ LeetCode/Easy/0191-number-of-1-bits/0191-number-of-1-bits.swift
@@ -1,12 +1,12 @@
 class Solution {
     func hammingWeight(_ n: Int) -> Int {
         // 정석: 2진수로 변환, 문자열 변환, 1 개수 세기
-        // optimization: 
+        // optimization: case 2(ii)
         
         // [2진수 변환]
-        // i) 2^31 나누고 나머지에 대해 2^30으로 나누고, ...
+        // i) 2^31 나누고 나머지에 대해 2^30으로 나누고, ... (비효율적, 형변환 필요)
         var count = 0
-        var remainder = n
+        // var remainder = n
         // var i = 31
         // while (remainder > 0 && i > 0) {
         //     remainder = remainder % Int(pow(2, i))
@@ -18,13 +18,14 @@ class Solution {
         // }
         
         // ii) bitwise 연산자
-        while remainder > 0 {
-            count += remainder & 1 // 마지막 비트가 1이면 count 증가
-            remainder >>= 1 // 오른쪽으로 한 비트 이동 // 1011 -> 101 -> 10 -> 1
+        var remainingBits = n
+        while remainingBits > 0 {
+            count += remainingBits & 1 // 마지막 비트가 1이면 count 증가
+            remainingBits >>= 1 // 오른쪽으로 한 비트 이동 // 1011 -> 101 -> 10 -> 1
         }
         return count
         
-        // iii) swift 특징 이용
+        // iii) swift 특징 이용 (not proper as an answer)
         // return String(n, radix: 2) // 2진수 변환
                 // .filter { $0 == "1" }.count
     }
