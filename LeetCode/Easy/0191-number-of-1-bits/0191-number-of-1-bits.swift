fb6554fb88af289030dea4dd31b2af7fbebfc349
Aiden <84381320+g0getter@users.noreply.github.com>
2025-02-18 07:15:30 +0900

    Time: 0 ms (100%), Space: 19.3 MB (53.07%) - LeetHub

--- LeetCode/Easy/0191-number-of-1-bits/0191-number-of-1-bits.swift
+++ LeetCode/Easy/0191-number-of-1-bits/0191-number-of-1-bits.swift
@@ -3,11 +3,29 @@ class Solution {
         // 정석: 2진수로 변환, 문자열 변환, 1 개수 세기
         // optimization: 
         
-        // 2진수 변환
-        // 2^31 나누고 나머지에 대해 2^30으로 나누고, ...
-        // or
-        // swift 특징 이용
-        return String(n, radix: 2) // 2진수 변환
-                .filter { $0 == "1" }.count
+        // [2진수 변환]
+        // i) 2^31 나누고 나머지에 대해 2^30으로 나누고, ...
+        var count = 0
+        var remainder = n
+        // var i = 31
+        // while (remainder > 0 && i > 0) {
+        //     remainder = remainder % Int(pow(2, i))
+        //     print(remainder)
+        //     if remainder != 0 {
+        //         count += 1
+        //     }
+        //     i-=1
+        // }
+        
+        // ii) bitwise 연산자
+        while remainder > 0 {
+            count += remainder & 1 // 마지막 비트가 1이면 count 증가
+            remainder >>= 1 // 오른쪽으로 한 비트 이동 // 1011 -> 101 -> 10 -> 1
+        }
+        return count
+        
+        // iii) swift 특징 이용
+        // return String(n, radix: 2) // 2진수 변환
+                // .filter { $0 == "1" }.count
     }
 }
