4e0a67e88b47ed83f688f3b532b4de4e74265f62
Aiden <84381320+g0getter@users.noreply.github.com>
2025-02-17 21:42:00 +0900

    Time: 4 ms (49.28%), Space: 20.7 MB (13.04%) - LeetHub

--- LeetCode/Easy/0326-power-of-three/0326-power-of-three.swift
+++ LeetCode/Easy/0326-power-of-three/0326-power-of-three.swift
@@ -1,6 +1,25 @@
 class Solution {
-    // 1. divide by three iteratively until it becomes 1
+    // Solution 2. without loops/recursion
     func isPowerOfThree(_ n: Int) -> Bool {
+        guard n > 0 else { return false }
+        
+        let result = logBase(Double(n), base: 3)
+        // 부동소수점 오차 방지
+        let roundedResult = round(result)
+        return pow(3, roundedResult) == Double(n)
+        // print(result)
+        // print(Int(exactly: result))
+        // return Int(exactly: result) != nil // if Double is exactly Int
+    }
+    
+    func logBase(_ x: Double, base: Double) -> Double {
+        return log(x) / log(base)
+    }
+}
+
+extension Solution {
+    // Solution 1. divide by three iteratively until it becomes 1
+    func isPowerOfThree_loop(_ n: Int) -> Bool {
         var share = Double(n)
         while share > 1 {
             share /= 3
