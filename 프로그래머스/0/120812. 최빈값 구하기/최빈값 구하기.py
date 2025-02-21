30c1ca3a53c40fb470028cf2837a0b6c91332e27
Aiden <84381320+g0getter@users.noreply.github.com>
2025-02-21 16:10:50 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.22 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,29 +1,3 @@
 def solution(array):
-    # 1. fill `count_list`
-    count_list = find_alphabet_occurrence_array(array)
-    
-    # 2. find the maximum occurred element
-    max_occurred_number = 0
-    has_multiple_maxes = False
-    for i in range(1, len(count_list)):
-    # or
-    # for i in range(len(count_list)):
-        if count_list[i] > count_list[max_occurred_number]:
-            max_occurred_number = i
-            has_multiple_maxes = False
-        elif count_list[i] == count_list[max_occurred_number]:
-            has_multiple_maxes = True
-
-    if has_multiple_maxes:
-        return -1
-    else:
-        return max_occurred_number
-    
-
-def find_alphabet_occurrence_array(array):
-    number_occurrence_array = [0] * 1000
-
-    for number in array:
-        number_occurrence_array[number] += 1
-
-    return number_occurrence_array
+    answer = 0
+    return answer
