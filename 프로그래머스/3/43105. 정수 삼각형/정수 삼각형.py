4809374f9bd8a400d5d2ed2742eb50aaa6cecd9f
Aiden <84381320+g0getter@users.noreply.github.com>
2025-03-06 17:19:03 +0900

    refactor: Update 정수 삼각형.py
    개선

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,27 +1,22 @@
 def solution(triangle):
     # 위치 k인 수 N까지 오는 길의 최댓값 M(k)
     # 정확히는, triangle[i][j] 인 수까지의 최댓값을 M(i, j ~~)이라 했을 때
-    # M(i, j위치의 수) = max(M(i-1, j-1 위치의 수), M(i-1, j 위치의 수)) + (i, j 위치의 수) (단, i >=1, 0<j<최대인덱스)
+    # M(i, j위치의 수) = max(M(i-1, j-1 위치의 수), M(i-1, j 위치의 수)) +(i, j 위치의 수) (단, i >=1, 0<j<최대인덱스)
     answer = 0
     max_triangle = triangle
     
-    for i, level_nums in enumerate(triangle):
-        for j, num in enumerate(level_nums):
-            
-            if i == 0:
-                max_triangle[i][j] = triangle[i][j]
-                continue
-                
+    for i in range(1, len(triangle)):
+        
+        for j in range(len(triangle[i])):
+            # if i == 0:
+            #     max_triangle[i][j] = triangle[i][j]
+            #     continue
             if j == 0:
                 max_triangle[i][j] = max_triangle[i-1][j] + triangle[i][j]
-            elif j == len(level_nums)-1:
+            # elif j == len(level_nums)-1:
+            elif j == len(triangle[i])-1:
                 max_triangle[i][j] = max_triangle[i-1][j-1] + triangle[i][j]
             else:
                 max_triangle[i][j] = max(max_triangle[i-1][j-1], max_triangle[i-1][j]) + triangle[i][j]
                 
-    # print(max_triangle)
-    
-    for last_max in max_triangle[len(max_triangle)-1]:
-        if answer < last_max:
-            answer = last_max
-    return answer
+    return max(max_triangle[-1])
