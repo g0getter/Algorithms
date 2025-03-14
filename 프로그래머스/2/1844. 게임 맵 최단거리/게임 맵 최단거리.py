e9392294be614fecb83ddc0e527fd52dfb8402c4
Aiden <84381320+g0getter@users.noreply.github.com>
2025-03-14 16:47:32 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.04 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,4 +1,5 @@
 from collections import deque
+
 # 뽑고(dequeue) visited 처리하고 주변 네 칸 valid면 넣고(enqueue)
 def solution(maps):
     # queue: roads to visit
@@ -6,7 +7,7 @@ def solution(maps):
     
     queue.append((0,0, 1)) # enqueue. (x,y,distance). distance: (x,y)까지의 거리
     
-    # 여러 길(경우의 수)을 쪼개야 함(지금은 answer이 합쳐져서 들어감) -> queue에 포함
+    # 여러 길(경우의 수)별로 이동 거리를 분리해야 함 -> queue에 포함
     while queue:
         current_x, current_y, current_distance = queue.popleft() # dequeue(여기로 이동)
         # maps[current_x][current_y] = 2 # visited 처리
@@ -20,24 +21,7 @@ def solution(maps):
             new_y = current_y + y
             if 0 <= new_x < len(maps) and 0 <= new_y < len(maps[0]) and maps[new_x][new_y] == 1:
                 queue.append((new_x, new_y, current_distance + 1))
-                maps[new_x][new_y] = 2 # visited 처리
-                # print(queue)
+                # visited 처리 - 중복 방지 위해 pop 할 때 말고 방문할 곳 넣을 때 미리 visited 처리
+                maps[new_x][new_y] = 2 
         
     return -1
-
-
-# # 양 4칸 중 valid index이고 길인 곳 모아서, 거기까지의 거리(distance+1)와 함께 반환
-# def get_valid_four_roads(position, distance, maps):
-#     dx = [-1, 1, 0, 0]
-#     dy = [0, 0, -1, 1]
-    
-#     valid_roads = deque()
-    
-#     for i in range(len(dx)):
-#         x, y = position
-#         new_x = x + dx[i]
-#         new_y = y + dy[i]
-#         if 0 <= new_x < len(maps) and 0 <= new_y < len(maps[0]) and maps[new_x][new_y] == 1:
-#             valid_roads.append((new_x, new_y, distance + 1))
-            
-    return valid_roads
