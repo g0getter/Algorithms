7f6f3ec28909572d9497a934f4e4673e9b7b0e1d
Aiden <84381320+g0getter@users.noreply.github.com>
2025-03-25 10:35:11 +0900

    Time: 2 ms (95.32%), Space: 19 MB (77.64%) - LeetHub

--- LeetCode/Easy/0506-relative-ranks/0506-relative-ranks.py
+++ LeetCode/Easy/0506-relative-ranks/0506-relative-ranks.py
@@ -11,22 +11,5 @@ class Solution:
         medals = ["Gold Medal", "Silver Medal", "Bronze Medal"]
         rank_mapping = {score: medals[i] if i < 3 else str(i + 1) for i, score in enumerate(score_sorted)}
 
-        # dict = {} 
-        # for i in range(len(score_sorted)):
-        #     result = ""
-        #     if i < 3:
-        #         result = medals[i]
-        #     else:
-        #         result = f"{i + 1}"
-
-        #     dict[score_sorted[i]] = result
-
-        # result = []
-
         # 3
         return [rank_mapping[score] for score in score]
-
-        # for s in score:
-        #     result.append(dict[s])
-
-        # return result
