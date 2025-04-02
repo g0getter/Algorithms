3411beff4febb7cb3228646ec27c3d01b125c532
Aiden <84381320+g0getter@users.noreply.github.com>
2025-04-02 10:59:41 +0900

    Title: Date to Date, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250402/Date to Date/date-to-date.py
+++ 250402/Date to Date/date-to-date.py
@@ -1,19 +1,16 @@
 m1, d1, m2, d2 = map(int, input().split())
 
-# Please write your code here.
-if m1 == m2:
-    print(d2-d1+1)
-else: # 월이 다를 경우
-
+# 상대적인 값(차) 말고 절대적인 값을 각각 구해서 뺄셈
+def num_days(m, d):
     days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    count = 0
 
-    days_remaining = days[m1]-d1 + 1
+    for month in range(m):
+        count += days[month]
+    
+    count += d # m월의 days
 
-    count = days_remaining # m1의 days
+    return count
 
-    for month in range(m1+1, m2):
-        count += days[month] # m1과 m2 사이의 days
+print(num_days(m2, d2) - num_days(m1, d1) + 1)
 
-    count += d2 # m2의 days
-
-    print(count)
