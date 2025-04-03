e323b25f444bfad8e749760346599a9282b68ef3
Aiden <84381320+g0getter@users.noreply.github.com>
2025-04-04 08:37:17 +0900

    Title: DateTime to DateTime, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250404/DateTime to DateTime/datetime-to-datetime.py
+++ 250404/DateTime to DateTime/datetime-to-datetime.py
@@ -1,21 +1,19 @@
 a, b, c = map(int, input().split())
 
-# 1. 예외 처리
+# 1. (X)예외 처리 - 예외 처리 필요 없음!!!!!!!!! 음수면 -1 하면 됨
 # 2. 각 분 구한 후 뺄셈
 
-# 11월 한정(11월부터 흐른 분 구하기)
+# 11월 한정(11월 0시 0분부터 흐른 분 구하기)
 def getMinutesOnNovember(day, hour, minute):
     return (day-1) * 24 * 60 +  hour * 60 + minute
+    # 사실 차를 구할거라 day-1도 안해도 되긴 하나, 11월 0시 0분부터 흐른 분을 구한다는 의미에서 둠
 
-if a == 11 and (b < 11 or b == 11 and c < 11):
-    print(-1)
-else:
-    print(getMinutesOnNovember(a, b, c) - getMinutesOnNovember(11, 11, 11))
+diff = getMinutesOnNovember(a, b, c) - getMinutesOnNovember(11, 11, 11)
+print(-1 if diff < 0 else diff)
 
-# [X] 필요 이상으로 로직이 들어간 함수 
+# (X)필요 이상으로 로직이 들어간 함수
 # def getMinutes(month, day, hour, minute):
-    # count = 0
-
     # days = [0, 31, 30, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     # # month
     # for m in month:
+    # ...
