8b9fccf86fc7c74e6c647e8f4c4736412ee7c5d8
Aiden <84381320+g0getter@users.noreply.github.com>
2025-04-04 08:59:50 +0900

    Title: 요일 맞추기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250404/요일 맞추기/guess-day-of-week.py
+++ 250404/요일 맞추기/guess-day-of-week.py
@@ -2,8 +2,10 @@ m1, d1, m2, d2 = map(int, input().split())
 
 # 시작
 def get_days(m, d):
+    days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
     count = 0
-    for month in range(m-1):
+    for month in range(m):
         count += days[month]
     
     count += d
@@ -13,7 +15,6 @@ def get_days(m, d):
 # 흐른 날짜 계산해서 + 월요일(0으로 지정) 후 % 7
 day = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
-days = [0, 31, 30, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 # 흐른 날짜 (음수도 나올 수 있음)
 diff = get_days(m2, d2) - get_days(m1, d1)
