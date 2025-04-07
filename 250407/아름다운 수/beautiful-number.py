bdbd75c203bff13fdd3c33191711872f85617d8f
Aiden <84381320+g0getter@users.noreply.github.com>
2025-04-07 11:38:15 +0900

    Title: 아름다운 수, Time: 67ms, Memory: 17MB, Status: Passed - Codetree

--- 250407/아름다운 수/beautiful-number.py
+++ 250407/아름다운 수/beautiful-number.py
@@ -1,6 +1,5 @@
 n = int(input())
 
-# Please write your code here.
 # Brute-force
 # 1. 모든 가능한 자릿수 생성
 # 2. 각각이 아름다운 수인지 판별
@@ -11,30 +10,54 @@ n = int(input())
 # 3. stack.popLast(), k와 같은지 확인
 #    1) k와 같 -> 계속함
 #    2) k와 안 같 -> return false
+# prefix에 1~4 하나씩 붙이는 함수
+
+def append_a_number(prefix):
+    return [f"{prefix}{i}" for i in range(1,5)]
+
+def all_numbers():
+
+    result = [""]
+
+    while len(result[0]) < n:
+        temp = result
+        result = []
+        for prefix in temp:
+            result += append_a_number(prefix)
+
+    return result
+
 
 # 1. 모든 가능한 자릿수 생성
-nums = []
+nums = all_numbers()
+count = 0
 
-# for i in range(1,5): -> n번 중첩
-number = ""
-# for i in range(1, 5):
-#     # 맨 앞이 i일 때 나올 수 있는 아이들
-#     children = get_children(start=f"{i}", length=n)
-#     for child in children:
-#         nums.append(f"{i}{child}")
-    
-# def get_children(starts, length):
-#     if length - len(start) == 1:
-#         return ~~
-    
-#     answer = []
-#     for child in range(1,5):
-#         answer.append(get_children(f"{start}{child}", length))
+# 2. 각각이 아름다운 수인지 판별
+# [아름다운 수 판별법]
+# 각 자리를 돌면서, 그 자리 숫자 k에 대해
+# 1. stack 확인
+# 2. if 비었 -> stack에 k를 k개 추가
+# 3. stack.popLast(), k와 같은지 확인
+#    1) k와 같 -> 계속함
+#    2) k와 안 같 -> return false
+# prefix에 1~4 하나씩 붙이는 함수
+def is_beautiful_number(number):
+    stack = []
 
-# 앞이 ~일 때 1~4 하나씩 붙이는 함수
-def append_a_number(prefix):
-    if len(prefix) == n:
-        return prefix
-    append_a_number([f"{prefix}{i}" for i in range(1,5)])
+    for char in number:
+        k = int(char)
+        if len(stack) == 0:
+            for _ in range(k):
+                stack.append(k)
+        
+        last_num = stack.pop()
+        if last_num != k:
+            return False
 
-print(append_a_number(""))
+    return len(stack) == 0
+
+for num in nums:
+    if is_beautiful_number(num):
+        count += 1
+
+print(count)
