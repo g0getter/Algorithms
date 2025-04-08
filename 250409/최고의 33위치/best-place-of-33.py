7d61c0774ae358c465e42260d3d8dd66e2283194
Aiden <84381320+g0getter@users.noreply.github.com>
2025-04-09 08:35:49 +0900

    Title: 최고의 33위치, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250409/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,23 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+def count_coins(r, c):
+    count = 0
+    if r + 2 >= n or c + 2 >= n:
+        return -1
+
+    for i in range(r, r+3):
+        for j in range(r, r+3):
+            if grid[i][j] == 1: count += 1
+
+    return count
+
+coins = []
+for i in range(n):
+    for j in range(n):
+        if i + 2 >= n or j + 2 >= n:
+            continue # break도 가능할 것
+        coins.append(count_coins(i, j))
+    
+print(max(coins))
