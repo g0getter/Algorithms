70b5ac2c6942af284836d4907403fb18f5ab0572
Aiden <84381320+g0getter@users.noreply.github.com>
2025-04-10 08:34:49 +0900

    Title: 트로미노, Time: 39ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250410/트로미노/tromino.py
@@ -0,0 +1,33 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+shapes = [
+    [(0,0), (0,1), (1,0)],
+    [(0,0), (0,1), (1,1)],
+    [(0,1), (1,0), (1,1)],
+    [(0,0), (0,1), (1,0)],
+    [(0,0), (0,1), (0,2)],
+    [(0,0), (1,0), (2,0)]
+]
+
+def iterate_grid(shape): # shape: [(0,0), (0,1), (1,0)]
+    max_sum = 0
+    # grid 돌면서, 자기 기준 shape 세개의 합 확인
+    for i in range(n):
+        for j in range(m):
+            sub_sum = 0
+            for dx, dy in shape: # (0,0)
+                if i + dx >= n or j + dy >= m:
+                    break # 이번 shape은 글렀음
+                sub_sum += grid[i+dx][j+dy] # 괜찮으면 합침
+            # 셋 다 정상이었다면
+            max_sum = max(max_sum, sub_sum) # 비교
+            
+    return max_sum
+
+max_total_sum = 0
+for shape in shapes:
+    max_total_sum = max(max_total_sum, iterate_grid(shape))
+
+print(max_total_sum)
