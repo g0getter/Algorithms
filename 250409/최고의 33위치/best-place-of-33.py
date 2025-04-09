c328cdd6a5ba972ee7a149ec699fa28fca42b19b
Aiden <84381320+g0getter@users.noreply.github.com>
2025-04-09 09:03:42 +0900

    Title: 최고의 33위치, Time: 40ms, Memory: 16MB, Status: Passed - Codetree

--- 250409/최고의 33위치/best-place-of-33.py
+++ 250409/최고의 33위치/best-place-of-33.py
@@ -1,10 +1,13 @@
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-# Please write your code here.
+# 1. (0,0)부터 시작
+#   - 끝점(가로, 세로 모두)이 넘지 않으면,
+#   - 개수 세어 max 확인
+# 2. 끝까지 가고 max 출력 
 def count_coins(r, c):
     count = 0
-    if r + 2 >= n or c + 2 >= n:
+    if r + 2 >= n or c + 2 >= n: # 기능 분리 위한 의도적 반복
         return -1
 
     for i in range(r, r+3):
@@ -13,11 +16,11 @@ def count_coins(r, c):
 
     return count
 
-coins = []
+max_coin = 0
 for i in range(n):
     for j in range(n):
         if i + 2 >= n or j + 2 >= n:
-            continue # break도 가능할 것
-        coins.append(count_coins(i, j))
+            continue # break도 가능하지만 헷갈릴 수 있음
+        max_coin = max(count_coins(i, j), max_coin)
     
-print(max(coins))
+print(max_coin)
