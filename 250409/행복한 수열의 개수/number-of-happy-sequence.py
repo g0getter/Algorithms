3f4f7de0a8af0c86d12c27503888b56582ab74d0
Aiden <84381320+g0getter@users.noreply.github.com>
2025-04-09 10:11:41 +0900

    Title: 행복한 수열의 개수, Time: 58ms, Memory: 17MB, Status: Passed - Codetree

--- 250409/행복한 수열의 개수/number-of-happy-sequence.py
+++ 250409/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,20 +1,23 @@
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-# Please write your code here.
-def count_happy_row():
+# 행에 대해 찾고, 열에 대해 찾고 합침
+def count_happy(is_row):
     count = 0
     for i in range(n):
         same_number_count = 1
         previous = -1
         for j in range(n):
-            # print(i, j)
-            if grid[i][j] == previous:
+            # i, j만 바뀌면 되는 것 -> x, y로 처리
+            x = i if is_row else j
+            y = j if is_row else i
+            # print(x, y)
+            if grid[x][y] == previous:
                 same_number_count += 1
             else:
                 same_number_count = 1
                 
-            previous = grid[i][j]
+            previous = grid[x][y]
 
             if same_number_count >= m:
                 count += 1
@@ -22,24 +25,4 @@ def count_happy_row():
 
     return count
 
-def count_happy_column():
-    count = 0
-    for j in range(n):
-        same_number_count = 1
-        previous = -1
-        for i in range(n):
-            # print(i, j)
-            if grid[i][j] == previous:
-                same_number_count += 1
-            else:
-                same_number_count = 1
-                
-            previous = grid[i][j]
-
-            if same_number_count >= m:
-                count += 1
-                break # 다음 줄로! 그래야 끊겨도 행복한 수열로 세어짐.
-
-    return count
-
-print(count_happy_row() + count_happy_column())
+print(count_happy(True) + count_happy(False))
