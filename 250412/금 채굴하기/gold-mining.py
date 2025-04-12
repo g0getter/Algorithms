b12c6e1c2745adae6766ff17d15f9d2578ad5af0
Aiden <84381320+g0getter@users.noreply.github.com>
2025-04-12 13:05:51 +0900

    Title: 금 채굴하기, Time: 1059ms, Memory: 25MB, Status: Time Limit Exceed - Codetree

--- 250412/금 채굴하기/gold-mining.py
+++ 250412/금 채굴하기/gold-mining.py
@@ -1,3 +1,4 @@
+# import time 
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
@@ -11,35 +12,27 @@ def get_relative_grid_current_k(x, y, k):
     
     # 부호 붙이기
     for x, y in abs_list:
-        abs_set.add((x*1, y*1))
-        abs_set.add((x*1, y* -1))
-        abs_set.add((x* -1, y*1))
-        abs_set.add((x* -1, y* -1))
-    # print(abs_set)
+        # abs_set.add((x*1, y*1))
+        # abs_set.add((x*1, y* -1))
+        # abs_set.add((x* -1, y*1))
+        # abs_set.add((x* -1, y* -1))
+        abs_set.update({
+            (x, y),
+            (-x, y),
+            (x, -y),
+            (-x, -y),
+        })
 
     return list(abs_set)
 
 # x, y 기준 k번 이내로 갈 수 있는 유효한 영역들
 def get_relative_grid_in_k(x, y, k):
     # k 작은 것들 다 포함함!!
-    if k == 0:
-        # return [(x,y)]
-        return [(0,0)]
-
-    # result 구하기(이번 판)
     abs_list = []
     for i in range(0,k+1):
-        # print("ADD")
-        # print("get_relative_grid_current_k",x,y,i)
-        # print(get_relative_grid_current_k(x, y, i))
-        # print("TO")
-        # print(abs_list)
-        # abs_list.append((i, k-i))
         abs_list += get_relative_grid_current_k(x, y, i)
     
-    # print("RESULT abs_list", abs_list)
     return abs_list
-    # return get_grid_in_k(x, y, k-1) + result
     
 
 # x, y 기준이고 범위 k일 때 내부의 금 개수
@@ -67,5 +60,21 @@ for k in range(2*n): # 0 ~ n-1 말고 2n까지(?!)
                 max_gold_count = max(max_gold_count, gold_count)
 
 print(max_gold_count)
+# # 시간 측정 시작
+# start_time = time.time()
 
-# print(get_relative_grid_in_k(1,1,2))
+# # 여기서부터 코드를 실행
+# max_gold_count = 0
+
+# for k in range(2*n): 
+#     for i in range(n):
+#         for j in range(n):
+#             gold_count = count_gold(i, j, k)
+#             if gold_count * m - calculate_cost(k) >= 0:
+#                 max_gold_count = max(max_gold_count, gold_count)
+
+# # 시간 측정 끝
+# end_time = time.time()
+
+# # 실행 시간 출력
+# print(f"Execution time: {end_time - start_time} seconds")
