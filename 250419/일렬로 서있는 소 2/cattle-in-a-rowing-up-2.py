010edbb13d751e4f9cfd327f5b8867cf0d9fc507
Aiden <84381320+g0getter@users.noreply.github.com>
2025-04-19 14:22:11 +0900

    Title: 일렬로 서있는 소 2, Time: 52ms, Memory: 17MB, Status: Passed - Codetree

--- 250419/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 250419/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -1,13 +1,20 @@
 N = int(input())
 A = list(map(int, input().split()))
 
+count = 0
+# Solution 1: 정석대로 구현
 # 1. 3개 선택, i < j < k 순으로
 # 2. 키 비교, 맞으면 count +1
-count = 0
 for i in range(N):
     for j in range(i+1, N):
         for k in range(j+1, N):
             if A[i] <= A[j] <= A[k]:
                 count += 1
 
+# Solution 2: 모듈 combinations 이용
+# from itertools import combinations
+# for ai, aj, ak in list(combinations(A, 3)):
+#     if ai <= aj <= ak:
+#         count += 1
+        
 print(count)
