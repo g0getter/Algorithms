641d5adfe9337e5814573553d63fb3790c6cb607
Aiden <84381320+g0getter@users.noreply.github.com>
2025-04-23 08:38:25 +0900

    Title: 이상한 진수 2, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250423/이상한 진수 2/awkward-digits-2.py
+++ 250423/이상한 진수 2/awkward-digits-2.py
@@ -2,28 +2,23 @@ a = input()
 
 # Solution1. 골라서 변경
 def solution1():
-    # number = 0
-    answer = a[:len(a)-1]+"0"
+    answer = a[:-1]+"0"
 
     for i, num in enumerate(a):
         if num == "0":
             answer = a[:i]+"1"+a[i+1:]
             break
-            # number = number * 2
-        # else:
             
     return answer
 
-def get_number(string):
+def convert_binary_to_decimal(string):
     number = 0
     for digit in string:
-        if digit == "1":
-            number = number * 2 + 1
-        else:
-            number = number * 2
+        number = number * 2 + int(digit)
 
     return number
 
 # Solution2. 완전 탐색
 
-print(get_number(solution1()))
+
+print(convert_binary_to_decimal(solution1()))
