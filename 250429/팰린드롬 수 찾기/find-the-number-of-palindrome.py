54c8e2268febc7f68f79317527993891765fe299
Aiden <84381320+g0getter@users.noreply.github.com>
2025-04-29 16:11:16 +0900

    Title: 팰린드롬 수 찾기, Time: 53ms, Memory: 17MB, Status: Passed - Codetree

--- 250429/팰린드롬 수 찾기/find-the-number-of-palindrome.py
+++ 250429/팰린드롬 수 찾기/find-the-number-of-palindrome.py
@@ -4,12 +4,16 @@ X, Y = map(int, input().split())
 # 팰린드롬 판단: 맨 앞, 맨 뒤에서 하나씩 앞으로 오며 같은지 확인
 def is_palindrom(num):
     num = str(num)
-    n = len(num)
-    for i in range(n//2): # i: 0~n//2(미포함)
-        if num[i] != num[-(i+1)]:
-            return False
+    # Solution1: for문 사용
+    # n = len(num)
+    # for i in range(n//2): # i: 0~n//2(미포함)
+    #     if num[i] != num[-(i+1)]:
+    #         return False
     
-    return True
+    # return True
+
+    # Solution2: 뒤집기
+    return num == num[::-1]
 
 count = 0
 for num in range(X, Y+1):
