e111659a6929888a028c524cb15f83060751547c
Aiden <84381320+g0getter@users.noreply.github.com>
2025-04-30 08:32:52 +0900

    Title: 야바위, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250430/야바위/ya-rock.py
+++ 250430/야바위/ya-rock.py
@@ -1,25 +1,21 @@
 n = int(input())
 moves = [tuple(map(int, input().split())) for _ in range(n)]# [(1, 2, 1)],...
-# a, b, c = zip(*moves)
-# a, b, c = list(a), list(b), list(c)
 
-def swap(a, b, cups):
-    temp = cups[a]
-    cups[a] = cups[b]
-    cups[b] = temp
-    return cups
+# 1, 2, 3 경우 전부 시도
+# bool 가진 List 생성, 흐름 그대로 구현
 
 max_score = 0
 for i in range(1,4):
-    cups = [False] * 4 # 1, 2, 3번 종이컵 그대로 사용 위해
-    cups[i] = True
+    cups = [0] * 4 # 1, 2, 3번 종이컵 그대로 사용 위해. 보기 편하게 bool 대신 0, 1 사용
+    cups[i] = 1
     score = 0
+
     for a, b, c in moves:
-        cups = swap(a, b, cups)
-        if cups[c] == True:
+        cups[a], cups[b] = cups[b], cups[a] # swap!
+
+        if cups[c]: # 0 or 1
             score += 1
+
     max_score = max(max_score, score)
 
 print(max_score)
-
-    
