503c885170d62b423b9853dd1ecdd13a4ada0a33
Aiden <84381320+g0getter@users.noreply.github.com>
2025-05-04 14:38:50 +0900

    Title: 가장 작은 x 찾기, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250504/가장 작은 x 찾기/find-smallest-x.py
+++ 250504/가장 작은 x 찾기/find-smallest-x.py
@@ -1,7 +1,7 @@
 n = int(input())
 ranges = [tuple(map(int, input().split())) for _ in range(n)]
-a, b = zip(*ranges)
-a, b = list(a), list(b)
+
+MAX_NUM = 10000
 
 # 1. 1부터 시작
 # 2. 조건 전부 만족 확인
@@ -9,22 +9,21 @@ a, b = list(a), list(b)
 #                    x -> 2부터 다시 시작
 # ...
 
-num = 1
-answer_found = False
-# num 증가
-while num <= ranges[0][1]/2:
-    num_modifying = num
-    # 
+# number이 문제의 조건(N개 판단)을 만족하는지 반환
+def is_answer(number):
     for a, b in ranges:
-        num_modifying *= 2
-        if a <= num_modifying <= b:
-            answer_found = True
+        number *= 2
+        if a <= number <= b:
             continue
         else:
-            num += 1
-            answer_found = False
-            break
-        
-    if answer_found:
+            return False ##
+
+    return True ##
+
+# num 증가 담당
+for num in range(1, MAX_NUM+1):
+    if is_answer(num):
         print(num)
         break
+
+# 분기 로직이 애매하고 생각할 게 많아서 복잡해지면 함수로 분리해보기.
