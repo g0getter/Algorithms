c240578a805570d9782bc4cbd5c5afba1d40d928
Aiden <84381320+g0getter@users.noreply.github.com>
2025-05-11 22:14:08 +0900

    Title: 훌륭한 점프, Time: 56ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250511/훌륭한 점프/great-jump.py
+++ 250511/훌륭한 점프/great-jump.py
@@ -10,8 +10,9 @@ def is_max_possible(max_num):
             available_indices.append(i)
 
     # available_indices가 가능한지 검증
+    length = len(available_indices)
     # 맨 앞, 맨 뒤 포함 x 시 return False
-    if 0 not in available_indices or 1 not in available_indices:
+    if 0 not in available_indices or length-1 not in available_indices:
         return False
 
     for i in range(len(available_indices)-1): #길이 2면 i는 0까지 들어가야 함
