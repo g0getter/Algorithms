3198e6aed83afce59cb254048420977e5b1957cc
Aiden <84381320+g0getter@users.noreply.github.com>
2025-05-13 09:45:27 +0900

    Title: 사각형의 총 넓이 2, Time: 59ms, Memory: 17MB, Status: Passed - Codetree

--- 250513/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 250513/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -11,7 +11,8 @@ for _ in range(n):
 # 2. true로 변경 (x1,y1)~(x2-1, y2-1)
 # 3. true인 것 세어서 반환 (min x, min y) ~ (max x, max y)
 
-arr = [ ([False] * (max(x1+x2)+1)) for _ in range(max(y1+y2)+1) ] # 1
+# arr = [ ([False] * (max(x1+x2)+1)) for _ in range(max(y1+y2)+1) ] # 1
+coords_set = set()
 # FIXME: +1은 왜?
 
 # 직사각형 하나당
@@ -19,15 +20,17 @@ for xa, ya, xb, yb in zip(x1, y1, x2, y2):
     # 2
     for x in range(min(xa, xb), max(xa, xb)):
         for y in range(min(ya, yb), max(ya, yb)):
-            arr[x][y] = True
+            # arr[x][y] = True
+            coords_set.add((x,y))
 
 # 3
-count = 0
-for i in range(len(arr)):
-    for j in range(len(arr[0])):
-        count += 1 if arr[i][j] else 0
+# count = 0
+# for i in range(len(arr)):
+#     for j in range(len(arr[0])):
+#         count += 1 if arr[i][j] else 0
+print(len(coords_set))
 
 # for i in range(len(arr)):
 #     print(arr[i])
 
-print(count)
+# print(count)
