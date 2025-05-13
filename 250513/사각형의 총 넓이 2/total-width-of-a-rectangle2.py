2131c50e8fa999b8f616da8ea0d630dad39c085a
Aiden <84381320+g0getter@users.noreply.github.com>
2025-05-13 14:03:48 +0900

    Title: 사각형의 총 넓이 2, Time: 57ms, Memory: 17MB, Status: Passed - Codetree

--- 250513/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 250513/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -10,19 +10,18 @@ rects = [
 # 2. true로 변경 (x1,y1)~(x2-1, y2-1)
 # 3. true인 것 세어서 반환 (min x, min y) ~ (max x, max y)
 
-# arr = [ ([False] * (max(x1+x2)+1)) for _ in range(max(y1+y2)+1) ] # 1
-# 배열 만들 필요 없이, SET 사용하면 간단하고 빠름. (길이도 한 번에 구해짐!)
-coords_set = set()
+# 이차원 배열 만들 필요 없이, SET 사용하면 간단하고 빠름. (길이도 한 번에 구해짐!)
+# coords_set = set()
 
-# 직사각형 하나당
-for x1, y1, x2, y2 in rects: 
-    # 2
-    for x in range(x1, x2): # 문제에서 x1 < x2라 함
-        for y in range(y1, y2): # 문제에서 y1 < y2라 함
-            coords_set.add((x,y))
+# # 직사각형 하나당
+# for x1, y1, x2, y2 in rects: 
+#     # 2
+#     for x in range(x1, x2): # 개선2. 문제에서 x1 < x2라 함
+#         for y in range(y1, y2): # 문제에서 y1 < y2라 함
+#             coords_set.add((x,y))
 
-# 3
-print(len(coords_set))
+# # 3
+# print(len(coords_set))
 
 # Solution2. 기록 위해 이차원 배열 이용 (해설)
 # 1. 전부 양수로 만들기 위해 전부 100 더함
@@ -30,31 +29,31 @@ print(len(coords_set))
 # 3. 색칠된(true) 부분 개수 세기
 ## 주의: (0,0)부터 (1,1) -> 넓이 1임. (x2-1, y2-1)까지만 칠해야 함
 
-# MAX_R = 200 # 이차원 배열 크기
-# OFFSET = 100 # 양수로 만들기 위해 더할 offset
+MAX_R = 200 # 이차원 배열 크기
+OFFSET = 100 # 양수로 만들기 위해 더할 offset
 
-# arr = [
-#    [False] * MAX_R
-#    for _ in range(MAX_R) 
-# ]
+arr = [
+   [False] * MAX_R
+   for _ in range(MAX_R) 
+]
 
-# # 1
-# for x1, y1, x2, y2 in rects:
-#     # 2. 색칠
-#     x1 += OFFSET
-#     y1 += OFFSET
-#     x2 += OFFSET
-#     y2 += OFFSET
+# 1
+for x1, y1, x2, y2 in rects:
+    # 2. 색칠
+    x1 += OFFSET
+    y1 += OFFSET
+    x2 += OFFSET
+    y2 += OFFSET
 
-#     for x in range(x1, x2):
-#         for y in range(y1, y2):
-#             arr[x][y] = True
+    for x in range(x1, x2):
+        for y in range(y1, y2):
+            arr[x][y] = True
 
-# # 3 개수 세기
-# area = 0
-# for x in range(MAX_R):
-#     for y in range(MAX_R):
-#         if arr[x][y]:
-#             area += 1
+# 3 개수 세기
+area = 0
+for x in range(MAX_R):
+    for y in range(MAX_R):
+        if arr[x][y]:
+            area += 1
     
-# print(area)
+print(area)
