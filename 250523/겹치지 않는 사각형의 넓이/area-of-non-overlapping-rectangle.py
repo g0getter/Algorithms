9072343cb4f10c41e8414f737b64c8727d19baf7
Aiden <84381320+g0getter@users.noreply.github.com>
2025-05-23 16:31:23 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 89ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250523/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
+++ 250523/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
@@ -1,13 +1,20 @@
-x1 = [0] * 3
-y1 = [0] * 3
-x2 = [0] * 3
-y2 = [0] * 3
+# x1 = [0] * 3
+# y1 = [0] * 3
+# x2 = [0] * 3
+# y2 = [0] * 3
 
-x1[0], y1[0], x2[0], y2[0] = map(int, input().split())
-x1[1], y1[1], x2[1], y2[1] = map(int, input().split())
-x1[2], y1[2], x2[2], y2[2] = map(int, input().split())
+# x1[0], y1[0], x2[0], y2[0] = map(int, input().split())
+# x1[1], y1[1], x2[1], y2[1] = map(int, input().split())
+# x1[2], y1[2], x2[2], y2[2] = map(int, input().split())
 
-# TODO(확인): 공간 얼마로 만들지. 2000*2000?
+# tuple 형태의 배열로 받기
+n = 3
+rects = [
+    tuple(map(int, input().split())) # 위의 것 대신!!
+    for _ in range(n)
+]
+
+# ❗️ 공간 얼마로 만들지? 2000*2000
 # 1. 좌표 +1000해서 재조정
 # 2. 1로 채우기
 # 3. M은 다시 0으로 비우기
@@ -26,35 +33,21 @@ def print_coordinate(arr):
     for i in arr:
         print(i)
 
-OFFSET = 1000
+OFFSET = 10
+MAX_R = OFFSET * 2
+coordinate = [ [0] * (MAX_R+1) for _ in range(MAX_R+1) ]
 
-#1. 좌표 재조정
-for i, x in enumerate(x1):
-    x1[i] = x + 1000
+for i, (x1, y1, x2, y2) in enumerate(rects):
+    # 1. 좌표 재조정
+    x1, y1 = x1 + OFFSET, y1 + OFFSET
+    x2, y2 = x2 + OFFSET, y2 + OFFSET
 
-for i, x in enumerate(x2):
-    x2[i] = x + 1000
-    
-for i, y in enumerate(y1):
-    y1[i] = y + 1000
+    content = 1 if (i == 0 or i == 1) else 0
 
-for i, y in enumerate(y2):
-    y2[i] = y + 1000
-
-# print(x1, x2, y1, y2)
-
-coordinate = [ [0] * 2000 for _ in range(2000) ]
-# 2. 1로 채우기
-for i in range(0, 2):
-
-    for j in range(x1[i], x2[i]):
-        for k in range(y1[i], y2[i]):
-            coordinate[k][j] = 1 # TODO: 순서 확인
-
-# 3. 0으로 비우기
-    for j in range(x1[2], x2[2]):
-        for k in range(y1[2], y2[2]):
-            coordinate[k][j] = 0
-
+    # 2. 1로 채우기 & 3. 0으로 비우기 (순서/위치 따라)
+    for x in range(x1, x2):
+        for y in range(y1, y2):
+            coordinate[x][y] = content # 1, 2, 3을 차례로 채움
+            
+# 4. 1인 개수 세기
 print(count_one(coordinate))
-    
