e74038dc8ef318436d3ca6cec95762333b0376b5
Aiden <84381320+g0getter@users.noreply.github.com>
2025-05-29 10:17:21 +0900

    Title: 방향에 맞춰 최대로 움직이기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250529/방향에 맞춰 최대로 움직이기/max-movements-with-direction.py
+++ 250529/방향에 맞춰 최대로 움직이기/max-movements-with-direction.py
@@ -3,11 +3,20 @@ num = [list(map(int, input().split())) for _ in range(n)]
 move_dir = [list(map(int, input().split())) for _ in range(n)]
 r, c = map(int, input().split())
 
+# 1. backtracking 함수의 역할: 방향의 수 중 본인보다 큰 것 선택 + 전부 시도
+# 2. backtracking 함수 구성
+# def backtracking( , ):
+#     if 종료 조건_후보 수가 존재하지 않을 때:
+#         max 이동 횟수 업데이트
+#         return
+#     for 후보 in 후보들:
+#         backtracking( , , 이동횟수 + 1)
+#         복구_다른 후보로 넘어가도록
 
 max_move_num = 0
 # 가리키는 방향의 수 중 본인보다 큰 것 추리기 + 전부 시도
 # move_num: r, c 위치에 오기까지 움직인 횟수
-def try_all_candidates(r, c, move_num):
+def try_all_candidates(r, c, move_num): # backtracking 함수
     global max_move_num
     # 추리기
     biggers = figure_out_biggers(r, c)
@@ -17,7 +26,7 @@ def try_all_candidates(r, c, move_num):
         return
     for br, bc in biggers:
         try_all_candidates(br, bc, move_num + 1)
-        # 다음 후보 진행
+        # 다음 후보 진행(코드 불필요)
 
 # 가리키는 방향의 수 중 본인보다 큰 것을 반환
 def figure_out_biggers(r, c):
@@ -43,12 +52,10 @@ def convert_to_dr_dc(direction):
         (1, -1),
         (0, -1),
         (-1, -1)
-
     ]
 
     return directions[direction]
 
-    
 
 try_all_candidates(r-1, c-1, 0)
 print(max_move_num)
