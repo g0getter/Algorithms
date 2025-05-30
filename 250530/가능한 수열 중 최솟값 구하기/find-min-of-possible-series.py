551870d38434b418e180af87c9ae364d0c3adcc7
Aiden <84381320+g0getter@users.noreply.github.com>
2025-05-30 11:24:41 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250530/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
+++ 250530/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -2,13 +2,14 @@ n = int(input())
 
 # 4, 5, 6 중 하나 뽑아서 sequence의 다음 자릿수 채우고 반환 (-> n자릿수까지)
 def backtracking(sequence):
-    if len(str(sequence)) == n:
+    if len(sequence) == n:
         return sequence
     
-    for i in [4,5,6]:
-        if not adjacent_consecutive(f"{sequence}{i}"):
-            sequence = backtracking(f"{sequence}{i}")
-            return sequence
+    for i in ['4','5','6']:
+        if not adjacent_consecutive(sequence + i):
+            result = backtracking(sequence + i)
+            if result:
+                return result
     
 # 인접한 연속 부분 수열이 동일한지 여부 반환
 def adjacent_consecutive(sequence):
@@ -19,4 +20,4 @@ def adjacent_consecutive(sequence):
     
     return False
 
-print(backtracking(4))
+print(backtracking('4'))
