a07ff025cd2a7cfb090ae88b460c83f1f9c79763
Aiden <84381320+g0getter@users.noreply.github.com>
2025-06-04 09:19:24 +0900

    Title: 동전 더하기, Time: 40ms, Memory: 16MB, Status: Passed - Codetree

--- 250604/동전 더하기/add-coins.py
+++ 250604/동전 더하기/add-coins.py
@@ -7,19 +7,20 @@ coins = [int(input()) for _ in range(n)]
 current_sum = 0
 coin_count = 0
 
-i = len(coins)-1
-while current_sum != k and i >= 0:
-    q = (k-current_sum) // coins[i] # 몫
-    if q > 0:
-        current_sum += coins[i] * q
-        coin_count += q
-        # print(current_sum, coins[i])
-    i -= 1
+## Solution1. 뒤에서 index로 접근, while 사용하기 때문에 직접 i 감소 필요, if q > 0 검증
+# i = len(coins)-1
+# while current_sum != k and i >= 0:
+#     q = (k-current_sum) // coins[i] # 몫
+#     if q > 0:
+#         current_sum += coins[i] * q
+#         coin_count += q
+#         # print(current_sum, coins[i])
+#     i -= 1
 
+
+## Solution2. 뒤집은 배열에 순서대로 접근, 불필요한 if q > 0 검증 제외.
+for coin in coins[::-1]:
+    q = (k - current_sum) // coin
+    current_sum += coin * q 
+    coin_count += q
 print(coin_count)
-
-
-
-
-
-
