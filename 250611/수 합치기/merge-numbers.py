e1a353c8ea04cbda8925f35a76d938a6af6b4cad
Aiden <84381320+g0getter@users.noreply.github.com>
2025-06-11 09:38:25 +0900

    Title: 수 합치기, Time: 218ms, Memory: 23MB, Status: Passed - Codetree

--- 250611/수 합치기/merge-numbers.py
+++ 250611/수 합치기/merge-numbers.py
@@ -7,14 +7,17 @@ arr = list(map(int, input().split()))
 # 가장 작은 두 수 -> 매번 sort(O(N)) 후 앞 2개?
 # N = 10^5이므로 O(k*N)이더라도 10^8 아래(=1초)이므로 ㄱㅊ
 
-heapq.heapify(arr)
+heapq.heapify(arr) # 최소 힙 이용 - O(logN)에 최솟값 뽑을 수 있도록. sort시 O(N)
 cost = 0
 while(len(arr) > 1):
     # arr.sort(reverse=True) # O(N)
     # sub_sum = arr.pop() + arr.pop()
-    sub_sum = heapq.heappop(arr) + heapq.heappop(arr)
+    sub_sum = heapq.heappop(arr) + heapq.heappop(arr) 
     cost += sub_sum
     # arr.append(sub_sum)
     heapq.heappush(arr, sub_sum) # 합친 값 다시 추가
 
 print(cost)
+
+# Time complexity(heapq): O(N x log N)
+# Time complexity(그냥 sort): O(N^2 x log N)
